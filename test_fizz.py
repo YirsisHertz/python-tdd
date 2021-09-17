68f336ee006e38f05f68ac0109e66ef37537b7e5
YirsisHertz <yirsishertz02@gmail.com>
2021-09-16 23:19:12 -0500

    Buzz test

--- test_fizz.py
+++ test_fizz.py
@@ -1,6 +1,23 @@
 from Fizz import fizzbuzz
 import pytest
 
-@pytest.mark.parametrize("test_input, expected", [(3, "Fizz"), (6, "Fizz"), (9, "Fizz"), (12, "Fizz")])
+@pytest.mark.parametrize("test_input, expected", 
+        [
+            (3, "Fizz"), 
+            (6, "Fizz"), 
+            (9, "Fizz"), 
+            (12, "Fizz")]
+        )
 def test_debeImprimirFizzEnMultiplosDe3(test_input, expected):
+    assert fizzbuzz(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input, expected", 
+        [
+            (5, "Buzz"), 
+            (10, "Buzz"), 
+            (20, "Buzz"), 
+            (25, "Buzz")]
+        )
+def test_debeImprimirBuzzEnMultiplosDe5(test_input, expected):
     assert fizzbuzz(test_input) == expected
